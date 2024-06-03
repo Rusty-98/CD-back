5f57c5487c3b4c915c5294febbb633abc10b839b
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-06-03 22:19:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://cd-front-xi.vercel.app",
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     }
